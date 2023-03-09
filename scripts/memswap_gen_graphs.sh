69850811880044f3c941acea35aa4507cb578bc1
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2023-03-09 19:31:34 +0100

    more work on memswap post

--- scripts/memswap_gen_graphs.sh
+++ scripts/memswap_gen_graphs.sh
@@ -24,6 +24,16 @@ $WCCHARTS --type bar-vertical ../memcpy_util/time.csv --category sse --category 
 # all
 $WCCHARTS --type bar-vertical ../memcpy_util/time.csv --category swap_ranges --category generic --category avx_unroll --title-chart "memswap_all, time 4MB"  --values-unit us   --output $IMGDIR/memswap_all_time.png
 
+# size vs speed
+$WCCHARTS --type line --title-chart "size vs speed, gcc -O0"   ../memcpy_util/diff_sizes_gcc_O0_massage.csv   --output $IMGDIR/memswap_size_vs_speed_gcc_O0_all.png
+$WCCHARTS --type line --title-chart "size vs speed, clang -O0" ../memcpy_util/diff_sizes_clang_O0_massage.csv --output $IMGDIR/memswap_size_vs_speed_clang_O0_all.png
+$WCCHARTS --type line --title-chart "size vs speed, gcc -O2"   ../memcpy_util/diff_sizes_gcc_O2_massage.csv   --output $IMGDIR/memswap_size_vs_speed_gcc_O2_all.png
+$WCCHARTS --type line --title-chart "size vs speed, clang -O2" ../memcpy_util/diff_sizes_clang_O2_massage.csv --output $IMGDIR/memswap_size_vs_speed_clang_O2_all.png
+$WCCHARTS --type line --title-chart "size vs speed, gcc -O0"   ../memcpy_util/diff_sizes_gcc_O0_massage.csv   --x-range [0:12] --y-range [0:19] --no-category memcpy_only --output $IMGDIR/memswap_size_vs_speed_gcc_O0_under_12.png
+$WCCHARTS --type line --title-chart "size vs speed, clang -O0" ../memcpy_util/diff_sizes_clang_O0_massage.csv --x-range [0:12] --y-range [0:19] --no-category memcpy_only --output $IMGDIR/memswap_size_vs_speed_clang_O0_under_12.png
+$WCCHARTS --type line --title-chart "size vs speed, gcc -O2"   ../memcpy_util/diff_sizes_gcc_O2_massage.csv   --x-range [0:12] --y-range [0:30] --no-category memcpy_only --output $IMGDIR/memswap_size_vs_speed_gcc_O2_under_12.png
+$WCCHARTS --type line --title-chart "size vs speed, clang -O2" ../memcpy_util/diff_sizes_clang_O2_massage.csv --x-range [0:12] --y-range [0:30] --no-category memcpy_only --output $IMGDIR/memswap_size_vs_speed_clang_O2_under_12.png
+
 # codesize
 $WCCHARTS --type bar-vertical --set O0 ../memcpy_util/size.csv --title-chart "code size -O0" --values-unit byte --output $IMGDIR/code_size_O0.png
 $WCCHARTS --type bar-vertical --set Os ../memcpy_util/size.csv --title-chart "code size -Os" --values-unit byte --output $IMGDIR/code_size_Os.png
